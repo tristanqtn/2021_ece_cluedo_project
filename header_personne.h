81fff75a2dfe4cba67accd35971d61b84ea2993d
tristanqtn <77152870+tristanqtn@users.noreply.github.com>
2021-10-27 23:29:32 +0200

    Add files via upload

---
+++ header_personne.h
@@ -0,0 +1,31 @@
+/**< PREPROCESSOR >***************************************************************************************/
+#ifndef HEADER_PERSONNE_H_INCLUDED
+#define HEADER_PERSONNE_H_INCLUDED
+/*********************************************************************************************************/
+
+
+/**< CLASSES >********************************************************************************************/
+class Personne
+{
+    private:
+        std::string m_nom;
+        std::string m_prenom;
+
+    public:
+
+        Personne();
+        Personne(std::string _nom, std::string _prenom);
+        ~Personne();
+
+        std::string get_nom() const;
+        std::string get_prenom() const;
+
+        void set_nom(std::string value);
+        void set_prenom(std::string value);
+
+        void affichage() const;
+};
+/*********************************************************************************************************/
+
+
+#endif // HEADER_PERSONNE_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -8,5 +8,16 @@ int main()
 {
     srand(time(NULL));
 
+    setupAllegro();
+
+    Partie nouvelle;
+
+    nouvelle.creer_partie();
+    nouvelle.afficher_meurtrier();
+    std::cout << std::endl << std::endl;
+    nouvelle.afficher_deck();
+
+    allegro_exit();
+
     return 0;
 }END_OF_MAIN();
